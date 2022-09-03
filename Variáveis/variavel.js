72b5fa01c55a85b2cd868c76f35e287b2a73ba87
juliomatias-creat <cursosconursoedu@gmail.com>
2022-09-02 21:35:56 -0300

    qualquer

---
+++ Variáveis/variavel.js
@@ -0,0 +1,16 @@
+// criando e atribuindo valor a uma  
+// variável imutável(const)
+
+const nomeAluno = "Matias"
+
+// Executei esse arquivo através do terminal    
+// com o comando node + 'nome do arquivo'
+
+console.log("apelido")
+
+// Inseri as alterações no stage com o comando: git add .
+// Criei um marco:
+// git commit -m "reprodução do primeiro exercício da vídeo-aula"
+// Finalmente "upei" com o git push
+
+
