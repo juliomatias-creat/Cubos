70d7b76ade7cd5607d1717e022aad7f5ebddda2c
juliomatias-creat <cursosconursoedu@gmail.com>
2022-09-05 01:40:57 -0300

    concatenação com o +

---
+++ Variáveis/aritiméticos.js
@@ -0,0 +1,24 @@
+let x = 10, y = 2;
+
+let resultado = x + y; //12
+
+console.log(resultado);
+
+resultado = x - y; //8
+
+console.log(resultado);
+
+resultado = x * y; //20
+
+console.log(resultado);
+
+resultado = x / y; //5
+
+console.log(resultado);
+
+// a soma de 10 e 2 é 12
+
+console.log(`a soma de ${x} e ${y} é 12`);
+
+
+
