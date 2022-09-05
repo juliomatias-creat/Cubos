15005fe3d0a016d1b1cf6760d1a9ebe1f68f0b91
juliomatias-creat <cursosconursoedu@gmail.com>
2022-09-05 02:49:30 -0300

    Refiz o exercício 3 com a contenação usando +, mas já havia conseguido a evolção da starting template(no final);

--- Variáveis/aritiméticos.js
+++ Variáveis/aritiméticos.js
@@ -1,23 +1,22 @@
 let x = 10, y = 2;
 
 let resultado = x + y; //12
-
-console.log(resultado);
+let mensagem = " A soma de " + x + " e " + y + " é " + resultado;
+console.log(mensagem);
 
 resultado = x - y; //8
-
-console.log(resultado);
+mensagem = " A diferença de " + x + " e " + y + " é " + resultado;
+console.log(mensagem);
 
 resultado = x * y; //20
-
-console.log(resultado);
+mensagem = " A multiplicação de " + x + " e " + y + " é " + resultado;
+console.log(mensagem);
 
 resultado = x / y; //5
-
-console.log(resultado);
+mensagem = " A divisão de " + x + " e " + y + " é " + resultado;
+console.log(mensagem);
 
 // a soma de 10 e 2 é 12
 
 console.log(`a soma de ${x} e ${y} é 12`);
 
-. .
