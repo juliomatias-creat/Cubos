fe474792f2302de5969f4fb610b2299e0a8056f7
juliomatias-creat <cursosconursoedu@gmail.com>
2022-09-15 00:55:33 -0300

    Caso da negação !

--- ExVariáveisVídeos/ifElseCensura2.js
+++ ExVariáveisVídeos/ifElseCensura2.js
@@ -1,18 +1,29 @@
-// tem ingresso 
-// idade >= 16 OU estar acompanhado dos pais
 
-const temIngresso = false;
-const idade = 15;
+const temIngresso = true;
+const idade = 16;
 const censura = 16;
 const estaComOsPais = false;
 
-// Interessante: Podemos omitir a comparação de variáveis do tipo boleana
+// tem ingresso 
+// idade >= censura OU estar acompanhado dos pais
 
 
 if (temIngresso) {
     if (idade >= censura || estaComOsPais) {
         console.log("pode entrar");
-    } else ("barrada");
+    } else {
+        console.log("barrada");
+    }
 } else {
-    console.log("Barrada");
+    console.log("Barrada.");
 }
+
+//Interessante: Quanso a variável que estamos comparando é do tipo
+//Boleano, não precisa igualar a true porque o javascript já tenta
+//transformar em V ou F.
+//Então quando a variável é boleana, eu posso omitir, quando a intenção
+//é justamente saber se é verdadeiro.
+
+//Eu entendi que (temIngresso === true)
+//equivale a     (temIngresso).
+
