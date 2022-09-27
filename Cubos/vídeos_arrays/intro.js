6edf1730a513af31ff21ae357f544f1a6d68dc4e
juliomatias-creat <cursosconcursosedu@gmail.com>
2022-09-27 16:30:48 -0200

    Primeira parte

---
+++ Cubos/vídeos_arrays/intro.js
@@ -0,0 +1,9 @@
+// Arrays são conjunto de dados orndenados e de um mesmo tipo, armezenados numa única varíavel.
+// Isso mesmo, agora guardamos vários dados, como uma lista de nomes e para isso não é mais preciso criar 
+// uma variável para cada nome.
+
+const nome1 = 'Jose';
+const nome2 = 'Joao';
+const nome3 = 'Maria';
+
+const nomeDosEstudantes = ['Jose', 'Joao', 'Maria'];
