97f549fc765c34ad7327da1f08e9fd59bb0040d1
juliomatias-creat <cursosconcursosedu@gmail.com>
2022-09-28 13:58:53 -0200

    Elucidando o for-c

---
+++ Cubos/forDeC/for-de-c.js
@@ -0,0 +1,9 @@
+const nomeDeEstudante = ['Jose', 'Julio','Maria', 'Joana', 'Andre'];
+ 
+// for (let i = 0; i<5; i++) {
+// console.log(nomeDeEstudante[i]);
+// }
+
+for (let i = 0; i<nomeDeEstudante.length; i++) { // nomeDeEstudante.length retorna o tamnho da array
+    console.log(nomeDeEstudante[i]);
+    }
