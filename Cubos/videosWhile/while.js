f9bbdc8a9a42c952847e20cf802c3df4f1f0228d
juliomatias-creat <cursosconcursosedu@gmail.com>
2022-09-28 12:06:53 -0200

    while_1

---
+++ Cubos/videosWhile/while.js
@@ -0,0 +1,9 @@
+const nomeDosEstudantes = ['Jose', 'Joao', 'Maria', 'Julio', 'Adriana'];
+
+let indice = 0;
+
+while (indice < 5) { // Enquanto
+    console.log(nomeDosEstudantes[indice]);
+    indice++
+}
+
