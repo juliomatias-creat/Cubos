4273153f54dedb5d68942d0ef6ec8d8dec9ee5b9
juliomatias-creat <cursosconcursosedu@gmail.com>
2022-09-28 11:41:41 -0200

    exResolvido1Vídeo

---
+++ Cubos/vídeos_arrays/ExercícioResolvido.js
@@ -0,0 +1,24 @@
+// Crie uma array com 5 nomes de países
+const paisesAmercaLatina = ['Brasil', 'Argentina', 'Peru','Bolivia', 'Venezuela'];
+// // Adicione um país ao fim da lista
+// paisesAmercaLatina.push('Chile');
+// // Imprima a lista na tela
+// console.log(paisesAmercaLatina);
+// // Remova um país do fim da lista
+// paisesAmercaLatina.pop();
+// // Imprima a lista na tela
+// console.log(paisesAmercaLatina);
+// // Adicione um país ao início da lista
+// paisesAmercaLatina.unshift('Chile');
+// console.log(paisesAmercaLatina);
+// // Remova um país do início da lista
+// paisesAmercaLatina.shift();
+// console.log(paisesAmercaLatina);
+// // Imprima o último país da lista
+const ultimo = paisesAmercaLatina[paisesAmercaLatina.length - 1];
+console.log(ultimo);
+//console.log(paisesAmercaLatina[4]);
+// Imprima o segundo país da lista
+console.log(paisesAmercaLatina[1]);
+// Imprima o país de índice 2 na tela
+console.log(paisesAmercaLatina[2]);
--- Cubos/vídeos_arrays/intro.js
+++ Cubos/vídeos_arrays/intro.js
@@ -2,9 +2,12 @@
 // Isso mesmo, agora guardamos vários dados, como uma lista de nomes e para isso não é mais preciso criar 
 // uma variável para cada nome.
 
+//Como Criar e acessar um array
+
 // const nome1 = 'Jose';
 // const nome2 = 'Joao';
 // const nome3 = 'Maria';
+
 
 const nomeDosEstudantes = ['Jose', 'Joao', 'Maria', 'Julio', 'Adriana'];
 
