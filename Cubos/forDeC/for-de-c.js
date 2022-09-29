85c21e67663b1b8ffe1a4431321f416f8e3dc497
juliomatias-creat <cursosconcursosedu@gmail.com>
2022-09-29 13:54:37 -0200

    Conteém a diferença para o for of...

--- Cubos/forDeC/for-de-c.js
+++ Cubos/forDeC/for-de-c.js
@@ -7,3 +7,6 @@ const nomeDeEstudante = ['Jose', 'Julio','Maria', 'Joana', 'Andre'];
 for (let i = 0; i<nomeDeEstudante.length; i++) { // nomeDeEstudante.length retorna o tamnho da array
     console.log(nomeDeEstudante[i]);
     }
+    
+    // Executa o loop enquanto a condição for verdadeira, 
+    // porém você pode instanciar as variaveis contadoras dentro da estrutura do loop.
