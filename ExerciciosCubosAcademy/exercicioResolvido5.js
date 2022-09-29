09de53e4f39410d5adc477613a2e1bca30932174
juliomatias-creat <cursosconcursosedu@gmail.com>
2022-09-29 20:55:19 -0200

    Preciso estudar muiiiitoooo!!!

---
+++ ExerciciosCubosAcademy/exercicioResolvido5.js
@@ -0,0 +1,20 @@
+// Faça um programa que conte de 10 até 0(de trá pra frente mesmo).
+
+for (let i = 10; i >= 0; i-- ) {
+    console.log(i);
+}
+
+
+/*Meu código abaixo: Totalmente surreal de errado! rsrsrsrsrsr...
+const contagemRegressiva = 'dezSegundo';
+
+let contador = 10;
+
+for (let letra of contagemRegressiva) {
+    if (contagemRegressiva <= 10) {
+        contador--;
+    }
+}
+
+console.log(contador);*/
+
