e47c4e29ae2caf0ecc41711544f42575877c0262
juliomatias-creat <cursosconcursosedu@gmail.com>
2022-10-01 13:40:56 -0200

    Multiplos de 3 entre 0 e 500 resolvido com forDeC e while

---
+++ ExerciciosCubosAcademy/exercicios-backend-arrays-loops-master/04/multiploDe3.js
@@ -0,0 +1,13 @@
+// Faça um programa que imprima na tela todos os múltiplos de 3 compreendidos entre 0 e 500.
+
+// for (let multiplo = 0; multiplo < 500 ; multiplo += 3) {
+//     console.log(multiplo);
+// } 
+
+let numero = 0;
+
+while (numero < 500) {
+    console.log(numero);
+    numero += 3;
+}
+
