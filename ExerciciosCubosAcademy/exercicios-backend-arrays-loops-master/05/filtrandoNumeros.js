950f07489164d3eeca1285cb9a6d5773806f5f27
juliomatias-creat <cursosconcursosedu@gmail.com>
2022-10-01 16:09:07 -0200

    Exercício base de estudo...

--- ExerciciosCubosAcademy/exercicios-backend-arrays-loops-master/05/filtrandoNumeros.js
+++ ExerciciosCubosAcademy/exercicios-backend-arrays-loops-master/05/filtrandoNumeros.js
@@ -10,10 +10,32 @@ const original = [1, 4, 12, 21, 53, 88, 112, 114];
 let pares = [];
 
 for (let numero of original) {
-    const restoDaDivisaoPor2 = numero % 2;
+    const restoDaDivisaoPor2 = numero % 2;   // if (numero % 2 === 0)
     if (restoDaDivisaoPor2 === 0) {
         // é par
        pares.push(numero); // Insere no array de pares
 }
 
 } console.log(pares);
+
+// for (let numero of original) {
+//        if (numero % 2 === 0) {
+//         // é par
+//        pares.push(numero); // Insere no array de pares
+// }
+
+// } console.log(pares);
+
+// for (let i = 0; i < original.length; i++) {  //Com for de C
+//     if (original[i] % 2 === 0) {
+//         pares.push(original[i]);
+//     }
+// } console.log(pares);
+ 
+//  O for de C traz o valor do índice e sua posição
+
+//  O for of analisa cada elemento, salvando seu valor direto na variável let, mas não traz a sua posição.
+ 
+//  Logo, apesar do fo of ser mais prático, ele pode não ser a melhor alternativa que dependerá do caso.
+
+//  Se o problema quiser a posição, além do valor, o ideal é usar o forDeC
