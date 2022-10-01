30574fc55c693ac76f309df8d9fb129628c9f8ae
juliomatias-creat <cursosconcursosedu@gmail.com>
2022-09-30 22:08:38 -0200

    Verificando a quantidade de letras E ou e e imprimindo uma msg de acordo com a quantidade de letras encontradas

--- ExerciciosCubosAcademy/exercicios-backend-arrays-loops-master/02/index.js
+++ ExerciciosCubosAcademy/exercicios-backend-arrays-loops-master/02/index.js
@@ -1,1 +1,47 @@
+// seu codigo aqui
+
+// Faça um programa que conta quantas letras "E" ou "e" existem nesse array.
+
+// 1° para contar quantas letras 'E' ou 'e' existem, é preciso varrer a Array e para isso uso o for of
+
+// 2° É preciso verificar se cada letra é igaul a 'E' ou 'e".
+
+// a) Quando não houver nenhuma letra "E" ou "e" como no array letras, Deverá ser impresso no console:
+
+// Nenhuma letra "E" ou "e" foi encontrada.
+
+// const letras = ["A", "a", "B", "C", "L", "z"];
+
 const letras = ["A", "a", "B", "C", "E", "e"];
+
+let contador = 0;
+
+for (let letra of letras) {
+    if (letra === 'E' || letra === 'e') {
+        contador++
+    }
+}
+
+ if (contador === 0) {
+    // com scape:
+    console.log("Nenhuma letra \"E\" ou \"e\" foi encontrada.");
+ } else if (contador === 1) {
+    console.log('Foi encontrada uma letra "E" ou "e"');
+ } else {
+    console.log(`Foram encontradas ${contador} letras "E" ou "e".`);
+ }
+
+
+
+// let somaTotal = 0;
+// let encontrado = false;
+
+// for (let item of letras) {
+//     if (item === 'E' || item === 'e')
+//     somaTotal++;
+//     encontrado === true
+
+// }
+//  if (!encontrado) {
+//     console.log('Nenhuma letra "E" ou "e" foi encontrada.')
+//  }
