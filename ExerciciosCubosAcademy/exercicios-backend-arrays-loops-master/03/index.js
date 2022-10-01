02a3b4aafbda6c3dcf762d9874a71da8a9e1b6d2
juliomatias-creat <cursosconcursosedu@gmail.com>
2022-09-30 23:50:44 -0200

    ache o 10 e informe sua posição

--- ExerciciosCubosAcademy/exercicios-backend-arrays-loops-master/03/index.js
+++ ExerciciosCubosAcademy/exercicios-backend-arrays-loops-master/03/index.js
@@ -1,2 +1,16 @@
-const numeros = [54, 22, 14, 87, 10, 284];
+const numeros = [54, 22, 14, 87, 1, 6, 10, 284];
+
+// Faça um programa que verifica se existe o número 10 nesse array. Caso exista, 
+// informa a posição (índice) em que o número 10 se encontra. Caso não exista, deverá ser impresso -1.
+
+let resposta = -1;
+
+for (let i =0; i < numeros.length; i++) {
+    let numero = numeros[i];
+    if (numero === 10) {
+        resposta = i;
+        break;
+    }
+}
+console.log(resposta);
 
