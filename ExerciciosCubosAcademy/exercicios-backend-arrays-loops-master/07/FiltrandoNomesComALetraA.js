f8434c4c655d15739ac4574188d3ba91e6513f43
juliomatias-creat <cursosconcursosedu@gmail.com>
2022-10-01 18:20:29 -0200

    Excelente exercício, pegando apenas nomes com a letra A ou a

---
+++ ExerciciosCubosAcademy/exercicios-backend-arrays-loops-master/07/FiltrandoNomesComALetraA.js
@@ -0,0 +1,37 @@
+// Crie um novo array a partir do primeiro que contenha apenas os nomes que começam com a letra "A" ou "a". 
+// Ao final, imprima a variável que guarda o array.
+
+// percorrer array procurando "A" ou "a"
+// Se encontrar, inseri-las no novo array
+// imprima a variável que guarda o array.
+
+
+// const nomes = ["Ana", "Joana", "Carlos", "amanda"];
+// let novoArray = [];
+
+//     for (let nome of nomes) {
+//         if (nome[0] === "A"|| nome[0] === "a") {
+//         novoArray.push(nome);
+//     }
+    
+// } 
+
+// console.log(novoArray);
+
+const nomes = ["Joana", "Adalberto", "Carlos", "amanda", "Pedro", "Araújo"];
+
+let novoArray = [];
+
+    for (let nome of nomes) {
+        const inicial = nome[0];
+        if (inicial[0] === "A"|| inicial[0] === "a") {
+        novoArray.push(nome);
+    }
+    
+} 
+
+console.log(novoArray);
+
+
+
+    
