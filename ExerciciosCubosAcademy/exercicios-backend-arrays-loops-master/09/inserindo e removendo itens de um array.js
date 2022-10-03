f04a88906b74cbd6dd5a083fcd5398f3652990d0
juliomatias-creat <cursosconcursosedu@gmail.com>
2022-10-03 00:01:34 -0200

    manipulando arrays, inserindo e removendo itens

---
+++ ExerciciosCubosAcademy/exercicios-backend-arrays-loops-master/09/inserindo e removendo itens de um array.js
@@ -0,0 +1,14 @@
+
+const filaDePedidos = ["pedido 12", "pedido 13", "pedido 14"]; 
+
+// b) Retire o primeiro pedido "pedido 12" da fila.
+
+// c) Imprima no console a filaDePedidos
+
+filaDePedidos.push("pedido 15");
+
+filaDePedidos.shift()
+
+console.log(filaDePedidos);
+
+
